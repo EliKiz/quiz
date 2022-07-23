639a6fa207ca3993a0e8d8de0fd4a62063cd8ab6
Ilya <ilia0806280@gmail.com>
2022-07-23 17:29:42 +0400

    Adding progress bar and fixing bugs

--- main.js
+++ main.js
@@ -1,45 +1,12 @@
-const questions = [
-	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
-	},
-	{
-		question: "Что означает CSS?",
-		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
-		],
-		correct: 1,
-	},
-	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
-		correct: 2,
-	},
-];
-
-
-
 
 const titleQuize = document.querySelector('#header'),
 	listQuize = document.querySelector('#list'),
 	submitQuize = document.querySelector('#submit'),
 	loader = document.querySelector('.loader'),
 	game = document.querySelector('.quiz'),
-	categories = document.querySelector('#category');
+	categories = document.querySelector('#category'),
+	progressText = document.querySelector('.quiz-bar-text'),
+	progressBarFull = document.querySelector('.quiz-progress-bar-full');
 	
 
 	let score = 0; // кол-во правильных ответов
@@ -66,15 +33,21 @@ const titleQuize = document.querySelector('#header'),
 
 		switch(categories.value) { 
 			case '0': 
-			getData()
-				.then(()=>  {
-
+			url ='https://the-trivia-api.com/api/questions?categories=history&limit=5&region=US&difficulty=medium'
+			getData(url)
+			.then((data)=>  {
+					clearAnswer();
+					renderQuestion(data);	
+				})
+				.catch((error)=>  {
+					console.error(error)
 				});
 			break;
 			case '1': 
 			url ='https://the-trivia-api.com/api/questions?categories=music&limit=5&region=US&difficulty=medium'
 			getData(url)
 				.then((data)=>  {
+					clearAnswer();
 					renderQuestion(data);			
 				})
 				.catch((error)=>  {
@@ -85,6 +58,7 @@ const titleQuize = document.querySelector('#header'),
 			url ='https://the-trivia-api.com/api/questions?categories=science&limit=5&region=US&difficulty=medium'
 			getData(url)
 				.then((data)=>  {
+					clearAnswer();
 					renderQuestion(data);			
 				})
 				.catch((error)=>  {
@@ -95,6 +69,7 @@ const titleQuize = document.querySelector('#header'),
 			url ='https://the-trivia-api.com/api/questions?categories=geography&limit=5&region=US&difficulty=medium'
 			getData(url)
 				.then((data)=>  {
+					clearAnswer();
 					renderQuestion(data);			
 				})
 				.catch((error)=>  {
@@ -118,27 +93,25 @@ const titleQuize = document.querySelector('#header'),
 		if(!res.ok) { 
 			throw new Error(`could not fetch ${url} status ${res.status}`);
 		}
-		// console.log(data);
+	
 		return data;
 	};
-
-	// let Data = getData(url)
-	// 	.then((data) =>  {
-	// 		console.log(data)
-	// 		return data;
-	// 	})
-
-	// console.log(Data);
-
 	console.log('url is ', url)
 
 	getData(url)
 		.then((data) =>  {
-			// console.log(data);
+			console.log(data);
+			console.log('data[questionIndex].correctAnswer', data[questionIndex].correctAnswer);
 			// data.forEach(item => console.log(item))
 			// console.log(data[questionIndex].question); 
 			renderQuestion(data);
+			// checkAnswer();
 			
+			submitQuize.addEventListener('click', () =>  {
+				checkAnswer(data);
+
+			});
+
 			game.classList.remove('hidden');
 			loader.classList.add('hidden');
 		})
@@ -146,6 +119,8 @@ const titleQuize = document.querySelector('#header'),
 			console.error(error);
 		});
 
+	// console.log(getData());
+	
 	function clearAnswer() { 
 		titleQuize.innerHTML = '';
 		listQuize.innerHTML = '';
@@ -162,8 +137,11 @@ const titleQuize = document.querySelector('#header'),
 		answers.push(data[questionIndex].correctAnswer);
 
 		randomArrayShuffle(answers)
-
-		// console.log('data is 0', data[0]);
+		//progress bur 
+		questionIndex++;
+		progressText.innerHTML = `Question ${questionIndex}/${data.length-1}`;
+		console.log(`${(questionIndex / data.length) * 100}`);
+		progressBarFull.style.width = `${(questionIndex / data.length) * 100}%`;
 
 		titleQuize.innerHTML =  `
 		<h2 class="title">${data[questionIndex].question}</h2>
@@ -180,55 +158,49 @@ const titleQuize = document.querySelector('#header'),
 				</li>
 			`;
 		});
-
+		submitQuize.blur();
 		data[questionIndex];
 	}
 
-	function checkAnswer() { 
+	function checkAnswer(data) { 
 		
 		const inputQuize = listQuize.querySelector('input[type="radio"]:checked');
-		console.log(inputQuize.nextElementSibling.textContent)
-		getData(url)
+		// console.log(inputQuize.nextElementSibling.textContent);
+		console.log('data ISSSS', data);
 
-			.then((data) =>  {
+			if(!inputQuize && questionIndex !== data.length-1) { 
+				alert('Pleace select 1 answer')
+				submitQuize.blur();
+				return;
+			}
+			console.log(data);
+			
 
-				if(!inputQuize && questionIndex !== data.length-1) { 
-					alert('Пожалуйста выберите 1 вариант ответа')
-					submitQuize.blur();
-				}
-		
-				if(inputQuize.nextElementSibling.textContent === data[questionIndex].correctAnswer) {
-					console.log('done correct')
-					score++;
-					console.log(score)
-				}
+			if(inputQuize.nextElementSibling.textContent === data[questionIndex].correctAnswer) {
+				console.log('done correct')
+				score++;
+				console.log(score)
+			}
 
-				if(questionIndex !== data.length-1) { 
-					questionIndex++;
-					clearAnswer();	
-		
-					getData(url)
-					// loader.classList.remove('hidden')
-					.then((data) =>  {			
-						// game.parentNode.appendChild(loader);
-						renderQuestion(data);
-					});
-					
-				}else {
-					clearAnswer();
-					showResults();
-				}
+			if(questionIndex !== data.length-1) { 
 				
+				clearAnswer();	
+				renderQuestion(data);
 				
-				return data;
-			})
-			.catch((error)=> { 
-				console.error(error);
-			});
+			}else {
+				
+				clearAnswer();
+				showResults();
+			}
+			
+			
+			return data;
+			
 	}
 
-	function showResults() { 
 
+	function showResults() { 
+		
 		getData(url)
 		.then((data) =>  {
 				let title, message, result;
@@ -237,7 +209,7 @@ const titleQuize = document.querySelector('#header'),
 				if(score === data.length) { 
 					title = 'Congratulations'
 					message = 'You answered all the questions'
-				}else if ((score * 100) / data.length >=70) { 
+				}else if ((score * 100) / data.length >= 70) { 
 					title = 'Not bad result';
 					message = `You answered ${(score * 100) / data.length}%`;
 				}else{ 
@@ -245,12 +217,12 @@ const titleQuize = document.querySelector('#header'),
 					message = `You answered ${(score * 100) / data.length}%`;
 				}
 				
-				result = `${score} из ${data.length}`;
+				result = `${score} of ${data.length-1}`;
 				
-				const resultTemplate = `
-			<h2 class="title">${title}</h2>
-			<h3 class="summary">${message}</h3>
-			<p class="result">${result}</p>
+			const resultTemplate = `
+				<h2 class="title">${title}</h2>
+				<h3 class="summary">${message}</h3>
+				<p class="result">${result}</p>
 			`;
 
 			submitQuize.textContent = 'Try again';
@@ -261,17 +233,15 @@ const titleQuize = document.querySelector('#header'),
 			console.log(score);
 
 			titleQuize.innerHTML = resultTemplate;
-			
-			
+			console.log(`${(questionIndex / data.length) * 100}`);
+			questionIndex++;
+			progressBarFull.style.width = `${(questionIndex / data.length) * 100}%`;
 		});
 		
 			
 	}
 
-	submitQuize.addEventListener('click', () =>  {
-		checkAnswer();
-
-	});
+	
 
 
 
@@ -287,6 +257,39 @@ const titleQuize = document.querySelector('#header'),
 		return array;
 	  }
 
+
+
+	//   const reverseSeq = n => {
+	// 	// return n > 0 ? n-- : n.split("") ;
+	// 	let res = [];
+	// 	while(n>0) { 
+	// 		res.push(n--);
+	// 	}
+	// 	return res;
+	//   };
+
+	//   console.log(reverseSeq(5));
+	
+	//   function areYouPlayingBanjo(name) {
+	// 	let play =`${name} plays banjo`;
+	// 	let dose =`${name} does not play banjo`;
+	// 		for(let i = 0; i<name.length; i++) { 
+	// 		let ap =  name[i] === 'R' ||  name[i] === 'r' ?  play : dose ;
+	// 		//   if(name[i] === 'R' ||  name[i] === 'r') { 
+	// 		// 	return play
+	// 		//   }else {
+	// 		// 	return dose
+	// 		//   }
+	// 		return ap
+	// 		}
+		
+	//   }
+	//   console.log(areYouPlayingBanjo('Robvert'));
+
+	//   function invert(array) {
+	// 	return array.map((item) => -item );
+	// }
+	//  console.log(invert([1,2,-3,4,5])) ;
 
 	// function printerError(s) {
 	// 	let currentArr = [...s];
